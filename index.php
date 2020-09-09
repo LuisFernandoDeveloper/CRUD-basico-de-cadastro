f6915ec56f45fddc4c74f9bcfefa2ff85e25e4f0
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-09-09 02:48:10 -0300

    Projeto iniciado, foi adicionado bootstrap 4.5.2 e um form basico

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de cadastro</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+
+    <form action="">
+        <div class="form-group">
+            <label for="">Nome</label>
+            <input type="email" class="form-control" id="">
+        </div>
+        <div class="form-group">
+            <label for="">email</label>
+            <input type="email" class="form-control" id="">
+        </div>
+        <div class="form-group">
+            <label for="">Senha</label>
+            <input type="email" class="form-control" id="">
+        </div>
+    </form>    
+
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+
+</html>
